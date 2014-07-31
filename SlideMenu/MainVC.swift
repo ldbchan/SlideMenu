8042c9da724829a6acca7ed050e50d6d69bfbde1
littledoorboard <mrqul@hotmail.com>
2014-08-01 00:43:43 +0800

    first commit

---
+++ SlideMenu/MainVC.swift
@@ -0,0 +1,40 @@
+//
+//  MainVC.swift
+//  SlideMenu
+//
+//  Created by Chantil on 2014/8/1.
+//
+//
+
+import UIKit
+
+class MainVC : UIViewController {
+    var sidebarIsOpen: Bool?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sidebarIsOpen = false
+    }
+    
+    @IBAction func menuButtonClicked(sender : AnyObject) {
+        
+        var x = self.sidebarIsOpen! ? 0 : 274
+        UIView.animateWithDuration(0.2, animations: {
+            self.view.frame = CGRect(x:x, y:0, width:320, height:568)
+            }, completion: { _ in
+                self.sidebarIsOpen = !(self.sidebarIsOpen!)
+            })
+    }
+    
+    @IBAction func swipeHappend(recognizer : UISwipeGestureRecognizer) {
+        
+        var x = (recognizer.direction == .Left) ? 0 : 274
+        UIView.animateWithDuration(0.2, animations: {
+            self.view.frame = CGRect(x:x, y:0, width:320, height:568)
+            }, completion: { _ in
+                self.sidebarIsOpen = (recognizer.direction == .Left) ? false : true
+            })
+        
+    }
+}
---
+++ SlideMenu/SidebarVC.swift
@@ -0,0 +1,25 @@
+//
+//  SidebarVC.swift
+//  SlideMenu
+//
+//  Created by Chantil on 2014/7/31.
+//
+//
+
+import Foundation
+import UIKit
+
+class SidebarTableView : UITableView {
+    
+}
+
+class SidebarVC : UITableViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
